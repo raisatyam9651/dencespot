a7c5c10761742cebe61020ff5c871bd09e5b1bd3
BP <bp@BPs-MacBook-Air.local>
2026-02-03 17:37:48 +0530

    Refactor header structure and remove Dr Prachi from PRP pages

--- header.php
+++ header.php
@@ -2,24 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <!-- SEO Meta Tags -->
-    <title><?php echo isset($pageTitle) ? $pageTitle : 'Best Hair Transplant Clinic in Gurugram | DenceSpot'; ?></title>
-    <meta name="description"
-        content="<?php echo isset($pageDesc) ? $pageDesc : 'DenceSpot Clinic is a trusted hair transplant & skin care clinic in Gurugram offering advanced aesthetic treatments by expert doctors.'; ?>">
-    <link rel="canonical" href="<?php echo isset($pageCanonical) ? $pageCanonical : 'https://dencespot.com/'; ?>">
-
-    <!-- Google Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700&family=Open+Sans:wght@400;600&family=Poppins:wght@500;600;700&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css?v=2">
-
-    <!-- FontAwesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <?php include 'header-link.php'; ?>
 </head>
 
 <body>
